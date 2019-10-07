a2f1dd6115b5ccbde77e00dedbeb330c3a6660b8
Evelina <evelinawahlstrom18@gmail.com>
2019-10-07 17:48:01 +0200

    request-api

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import './App.css';
 import React, { Component } from 'react'
 //import { connect } from 'react-redux'
 import { Route } from 'react-router-dom'
-import Dogslist from './components/Dogslist'
+import DogsList from './components/DogsList'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <main>
-          <Route exact path="/" component={Dogslist} />
+          <Route exact path="/" component={DogsList} />
+          <DogsList />
         </main>
-                {/* <Dogslist /> */}
       </header>
     </div>
   );
--- src/components/Dogslist.js
+++ src/components/Dogslist.js
@@ -1,11 +1,29 @@
-import React, { PureComponent } from 'react'
+import React, {Component} from 'react'
+import request from 'superagent'
 
-export default class Dogslist extends PureComponent {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
+
+export default class DogsList extends Component {
+    state = { dogBreeds: null }
+  
+    componentDidMount() {
+      /*{DidMount MEANS the component is rendered into the real DOM. "It's showtime"}*/
+      request
+        .get('https://dog.ceo/api/breeds/list/all')
+        .then(response => {
+          const breeds = Object.keys(response.body.message)
+          // transforms the response.bodu.message into an array of dog breeds
+          this.updateBreeds(breeds)
+        })
+        .catch(console.error)
     }
-}
+  
+    updateBreeds(breeds) {
+      this.setState({
+        dogBreeds: breeds
+      })
+    }
+  
+    render() {
+      return <DogsList dogBreeds={this.state.dogBreeds} />
+    }
+  }
