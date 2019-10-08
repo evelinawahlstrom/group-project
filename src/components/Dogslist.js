8598bbff04e34f65f3ae73dd3be21dc4611a9954
seabelis <seabelis74@gmail.com>
2019-10-08 12:05:23 +0200

    attempted thunk

--- src/components/Dogslist.js
+++ src/components/Dogslist.js
@@ -2,28 +2,13 @@ import React, {Component} from 'react'
 import request from 'superagent'
 
 
-export default class DogsList extends Component {
-    state = { dogBreeds: null }
-  
-    componentDidMount() {
-      /*{DidMount MEANS the component is rendered into the real DOM. "It's showtime"}*/
-      request
-        .get('https://dog.ceo/api/breeds/list/all')
-        .then(response => {
-          const breeds = Object.keys(response.body.message)
-          // transforms the response.bodu.message into an array of dog breeds
-          this.updateBreeds(breeds)
-        })
-        .catch(console.error)
-    }
-  
-    updateBreeds(breeds) {
-      this.setState({
-        dogBreeds: breeds
-      })
-    }
-  
-    render() {
-      return <DogsList dogBreeds={this.state.dogBreeds} />
-    }
+export default function DogsList() {
+  return function (dispatch) {
+    request('https://dog.ceo/api/breeds/list/all')
+    .then(response => dispatch(setBreeds(response.body)))
+  }
+
+  render() {
+    return <DogsList dogBreeds={this.state.dogBreeds} />
+  }
   }
--- src/store.js
+++ src/store.js
@@ -1,9 +1,14 @@
-import { createStore } from 'redux'
 import reducer from './reducer'
+import ReduxThunk from 'redux-thunk'
+import { createStore, applyMiddleware } from 'redux'
 
-const enhancer = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
 
 const store = createStore(reducer, enhancer)
 
+const enhancer = compose (
+    applyMiddleware (ReduxThunk),
+    devTools 
+)
 
 export default store
